0e1ed29fc867b6e83d24fcf15682b39978e5ca42
Matteo Fordiani <matteofordiani@gmail.com>
2024-08-12 12:07:53 +0200

    cpp basics

---
+++ 01_first_program/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Enter your favorite number: ";
+    
+    int favoriteNumber; 
+
+    std::cin >> favoriteNumber;
+
+    std::cout << "OMG! " << favoriteNumber << " is my favorite number too!" << std::endl;
+    
+    return 0;
+}
